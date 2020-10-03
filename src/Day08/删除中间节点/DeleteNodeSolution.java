4938fa6674f6e7fa99602c50e5636e553d2ed369
Zhongger <973593026@qq.com>
2020-10-03 11:07:14 +0800

    (链表)删除中间节点

--- src/Day08/删除中间节点/DeleteNodeSolution.java
+++ src/Day08/删除中间节点/DeleteNodeSolution.java
@@ -7,6 +7,12 @@ public class DeleteNodeSolution {
         ListNode head = ListNode.getOneToFiveList();
 
     }
+
+    /**
+     * 狸猫换太子
+     * 把下一个节点的值赋给要删除的节点，然后删除下一个节点
+     * @param node
+     */
     public void deleteNode(ListNode node) {
         node.val=node.next.val;
         node.next=node.next.next;
