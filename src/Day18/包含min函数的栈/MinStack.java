5134f5b62b6b06c23ab37bac4e32fabb52ce1fc5
Zhongger <973593026@qq.com>
2020-10-19 19:42:06 +0800

    (栈)包含min函数的栈

---
+++ src/Day18/包含min函数的栈/MinStack.java
@@ -0,0 +1,34 @@
+package Day18.包含min函数的栈;
+
+import java.util.Stack;
+
+/**
+ * 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+ */
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int min() {
+        return minStack.peek();
+    }
+}
