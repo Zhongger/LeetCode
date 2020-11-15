fb4e67c1fb87c1fbc7bc4f2cceee15205f3b43a4
zhongmingyi <zhongmingyi@huya.com>
2020-11-15 11:02:42 +0800

    （Top100）打家劫舍

---
+++ src/Day23/打家劫舍/RobSolution.java
@@ -0,0 +1,30 @@
+package Day23.打家劫舍;
+
+/**
+ * Created by Zhong Mingyi on 2020/11/15.
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ */
+public class RobSolution {
+    public static void main(String[] args) {
+        RobSolution solution = new RobSolution();
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(solution.rob(nums));
+    }
+
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
+}
