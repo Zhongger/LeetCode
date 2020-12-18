6cbc26523f6cea4467879aac9e495e3fafb9d51c
Zhongger <973593026@qq.com>
2020-12-18 11:46:56 +0800

    （树）对称的二叉树(dfs做法)

--- src/Day30/对称的二叉树/IsSymmetricSolution.java
+++ src/Day30/对称的二叉树/IsSymmetricSolution.java
@@ -83,4 +83,21 @@ public class IsSymmetricSolution {
         helper2(root.right);
         helper2(root.left);
     }
+
+    //正确的做法：
+
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null)
+            return true;
+        return helper(root.left, root.right);
+    }
+
+    public boolean helper(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null)
+            return true;
+        if (root1 == null || root2 == null)
+            return false;
+        return root1.val == root2.val && helper(root1.left, root2.right) &&
+                helper(root1.right, root2.left);
+    }
 }
