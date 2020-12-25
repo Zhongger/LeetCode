2cb24cd3e9b46305834216d01ed39704d2321602
Zhongger <973593026@qq.com>
2020-12-25 11:09:06 +0800

    （树）二叉搜索树中的搜索

---
+++ src/Day36/二叉搜索树中的搜索/SearchBSTSolution.java
@@ -0,0 +1,19 @@
+package Day36.二叉搜索树中的搜索;
+
+import common.TreeNode;
+
+public class SearchBSTSolution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        if (root.val < val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+}
