f965a46ebe1a61d037a7e92d51e2c9f734b347b3
Zhongger <973593026@qq.com>
2021-01-09 18:00:49 +0800

    （飞书后端）买卖股票的最佳时机

---
+++ src/Day42/买卖股票的最佳时机/MaxProfitSolution.java
@@ -0,0 +1,16 @@
+package Day42.买卖股票的最佳时机;
+
+public class MaxProfitSolution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i]<min){
+                min=prices[i];
+            }else if (prices[i]-min>maxProfit){
+                maxProfit=prices[i]-min;
+            }
+        }
+        return maxProfit;
+    }
+}
