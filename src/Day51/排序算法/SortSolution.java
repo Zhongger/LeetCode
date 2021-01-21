2fcd43df775619585599b6e521d760f9ec70dd2e
Zhongger <973593026@qq.com>
2021-01-21 11:54:46 +0800

    （飞书后端三面）排序算法

---
+++ src/Day51/排序算法/SortSolution.java
@@ -0,0 +1,238 @@
+package Day51.排序算法;
+
+import java.util.Arrays;
+
+public class SortSolution {
+    public static void main(String[] args) {
+        int[] array2 = {1, 2, 3, 4, 5};
+        int[] array = {50, 10, 90, 30, 70, 40, 80, 60, 20};
+        heapSort(array);
+        heapSort(array2);
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array2));
+    }
+
+    /**
+     * 冒泡排序 最佳情况：T(n) = O(n)   最差情况：T(n) = O(n^2)   平均情况：T(n) = O(n^2)
+     */
+    public static int[] bubbleSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j + 1] < array[j]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+
+    /**
+     * 选择排序 最佳情况：T(n) = O(n^2)   最差情况：T(n) = O(n^2)   平均情况：T(n) = O(n^2)
+     *
+     * @param array
+     * @return
+     */
+    public static int[] selectionSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[minIndex]) //找到最小的数
+                    minIndex = j; //将最小数的索引保存
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+
+    /**
+     * 插入排序  最佳情况：T(n) = O(n)   最差情况：T(n) = O(n^2)   平均情况：T(n) = O(n^2)
+     *
+     * @param array
+     * @return
+     */
+    public static int[] insertionSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        int current;
+        for (int i = 0; i < array.length - 1; i++) {
+            current = array[i + 1];
+            int preIndex = i;
+            while (preIndex >= 0 && current < array[preIndex]) {
+                array[preIndex + 1] = array[preIndex];
+                preIndex--;
+            }
+            array[preIndex + 1] = current;
+        }
+        return array;
+    }
+
+    /**
+     * 希尔排序 最佳情况：T(n) = O(nlogn)  最坏情况：T(n) = O(nlogn)  平均情况：T(n) =O(nlogn)
+     *
+     * @param array
+     * @return
+     */
+    public static int[] ShellSort(int[] array) {
+        int len = array.length;
+        int temp, gap = len / 2;
+        while (gap > 0) {
+            for (int i = gap; i < len; i++) {
+                temp = array[i];
+                int preIndex = i - gap;
+                while (preIndex >= 0 && array[preIndex] > temp) {
+                    array[preIndex + gap] = array[preIndex];
+                    preIndex -= gap;
+                }
+                array[preIndex + gap] = temp;
+            }
+            gap /= 2;
+        }
+        return array;
+    }
+
+
+    /**
+     * 归并排序 T(n) = O(n)  最差情况：T(n) = O(nlogn)  平均情况：T(n) = O(nlogn)
+     *
+     * @param array
+     * @return
+     */
+    public static int[] MergeSort(int[] array) {
+        if (array.length < 2) return array;
+        int mid = array.length / 2;
+        int[] left = Arrays.copyOfRange(array, 0, mid);
+        int[] right = Arrays.copyOfRange(array, mid, array.length);
+        return merge(MergeSort(left), MergeSort(right));
+    }
+
+    /**
+     * 归并排序——将两段排序好的数组结合成一个排序数组
+     *
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        for (int index = 0, i = 0, j = 0; index < result.length; index++) {
+            if (i >= left.length)
+                result[index] = right[j++];
+            else if (j >= right.length)
+                result[index] = left[i++];
+            else if (left[i] > right[j])
+                result[index] = right[j++];
+            else
+                result[index] = left[i++];
+        }
+        return result;
+    }
+
+    /**
+     * 快速排序方法 T(n) = O(nlogn)   最差情况：T(n) = O(n2)   平均情况：T(n) = O(nlogn)
+     *
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int[] QuickSort(int[] array, int start, int end) {
+        if (array.length < 1 || start < 0 || end >= array.length || start > end) return null;
+        int smallIndex = partition(array, start, end);
+        if (smallIndex > start)
+            QuickSort(array, start, smallIndex - 1);
+        if (smallIndex < end)
+            QuickSort(array, smallIndex + 1, end);
+        return array;
+    }
+
+    /**
+     * 快速排序算法——partition
+     *
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int partition(int[] array, int start, int end) {
+        int pivot = (int) (start + Math.random() * (end - start + 1));
+        int smallIndex = start - 1;
+        swap(array, pivot, end);
+        for (int i = start; i <= end; i++)
+            if (array[i] <= array[end]) {
+                smallIndex++;
+                if (i > smallIndex)
+                    swap(array, i, smallIndex);
+            }
+        return smallIndex;
+    }
+
+
+    /**
+     * 堆排序
+     */
+    public static void heapSort(int[] arr) {
+        // 将待排序的序列构建成一个大顶堆
+        for (int i = arr.length / 2; i >= 0; i--) {
+            heapAdjust(arr, i, arr.length);
+        }
+
+        // 逐步将每个最大值的根节点与末尾元素交换，并且再调整二叉树，使其成为大顶堆
+        for (int i = arr.length - 1; i > 0; i--) {
+            swap(arr, 0, i); // 将堆顶记录和当前未经排序子序列的最后一个记录交换
+            heapAdjust(arr, 0, i); // 交换之后，需要重新检查堆是否符合大顶堆，不符合则要调整
+        }
+    }
+
+    /**
+     * 构建堆的过程
+     *
+     * @param arr 需要排序的数组
+     * @param i   需要构建堆的根节点的序号
+     * @param n   数组的长度
+     */
+    private static void heapAdjust(int[] arr, int i, int n) {
+        int child;
+        int father;
+        for (father = arr[i]; leftChild(i) < n; i = child) {
+            child = leftChild(i);
+
+            // 如果左子树小于右子树，则需要比较右子树和父节点
+            if (child != n - 1 && arr[child] < arr[child + 1]) {
+                child++; // 序号增1，指向右子树
+            }
+
+            // 如果父节点小于孩子结点，则需要交换
+            if (father < arr[child]) {
+                arr[i] = arr[child];
+            } else {
+                break; // 大顶堆结构未被破坏，不需要调整
+            }
+        }
+        arr[i] = father;
+    }
+
+    // 获取到左孩子结点
+    private static int leftChild(int i) {
+        return 2 * i + 1;
+    }
+
+    /**
+     * 交换数组内两个元素
+     */
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+}
