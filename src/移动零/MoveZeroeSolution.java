4af70dd83f55d001e352222a03d0fcc93925ec89
Zhongger <973593026@qq.com>
2021-09-04 12:12:15 +0800

    chore: 移动零

---
+++ src/移动零/MoveZeroeSolution.java
@@ -0,0 +1,24 @@
+package 移动零;
+
+/**
+ * @author zhongmingyi
+ * @date 2021/9/4 11:48 上午
+ */
+public class MoveZeroeSolution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
