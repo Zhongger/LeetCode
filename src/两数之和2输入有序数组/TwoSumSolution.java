c0095ef74f3acb3a49cd2f6b5a2b5f9c41142b81
Zhongger <973593026@qq.com>
2021-09-04 13:01:19 +0800

    chore: 两数之和2输入有序数组

---
+++ src/两数之和2输入有序数组/TwoSumSolution.java
@@ -0,0 +1,23 @@
+package 两数之和2输入有序数组;
+
+/**
+ * @author zhongmingyi
+ * @date 2021/9/4 12:13 下午
+ */
+public class TwoSumSolution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
