ddc715601d6bce19d7f088254a79342e0df044e1
Zhongger <973593026@qq.com>
2021-10-01 23:30:11 +0800

    chore: 排序链表

---
+++ src/排序链表/SortListSolution.java
@@ -0,0 +1,78 @@
+package 排序链表;
+
+import common.ListNode;
+
+/**
+ * @author zhongmingyi
+ * @date 2021/9/29 3:06 下午
+ */
+public class SortListSolution {
+    public static void main(String[] args) {
+        SortListSolution solution = new SortListSolution();
+        ListNode head1 = new ListNode(4);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(1);
+        head1.next.next.next = new ListNode(3);
+
+
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(2);
+        head2.next.next = new ListNode(3);
+        head2.next.next.next = new ListNode(4);
+
+
+        ListNode res1 = solution.sortList(head1);
+        ListNode.printNode(res1);
+    }
+
+    public ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
+
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode l1 = sortList(head, mid);
+        ListNode l2 = sortList(mid, tail);
+        ListNode sorted = merge(l1, l2);
+        return sorted;
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 == null) {
+            cur.next = l2;
+        }
+        if (l2 == null) {
+            cur.next = l1;
+        }
+
+        return head.next;
+    }
+}
